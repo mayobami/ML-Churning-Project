786911576a56d21416cc093c26f2c8984bc3dc8d
Godimpression <marvelousayobami1@gmail.com>
2026-01-26 22:18:07 +0000

    Deploying on Flask web service

---
+++ Predict-test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+## a new customer informations
+customer = {
+  'customerid': '8879-zkjof',
+  'gender': 'female',
+  'seniorcitizen': 0,
+  'partner': 'no',
+  'dependents': 'no',
+  'tenure': 41,
+  'phoneservice': 'yes',
+  'multiplelines': 'no',
+  'internetservice': 'dsl',
+  'onlinesecurity': 'yes',
+  'onlinebackup': 'no',
+  'deviceprotection': 'yes',
+  'techsupport': 'yes',
+  'streamingtv': 'yes',
+  'streamingmovies': 'yes',
+  'contract': 'one_year',
+  'paperlessbilling': 'yes',
+  'paymentmethod': 'bank_transfer_(automatic)',
+  'monthlycharges': 79.85,
+  'totalcharges': 3320.75
+}
+import requests ## to use the POST method we use a library named requests
+url = 'http://localhost:9696/predict' ## this is the route we made for prediction
+response = requests.post(url, json=customer) ## post the customer information in json format
+result = response.json() ## get the server response. Without .json() it jsut shows 200-success, but json makes a dict to be returned
+print(result)
+
+
+# In[ ]:
+
+
+
+
